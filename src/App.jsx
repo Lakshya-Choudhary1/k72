58a9c827636ecfd2e2bcd2a612b2812972a6ed80
LAKSHYA CHOUDHARY <choudharylakshya.lc.com>
2026-05-16 07:21:42 +0530

    correcting naming

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 
 import { Home } from "./pages/Home.jsx";
 import  Agency  from "./pages/Agency.jsx";
-import { Work } from "./pages/Work.jsxork";
+import { Work } from "./pages/Work.jsx";
 
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
